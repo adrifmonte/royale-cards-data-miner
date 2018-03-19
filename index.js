ab54bf3db01a2e8dd43a63b2a9d32f5a23086da6
Adriano Falsarella Monte <adrifmonte@gmail.com>
2018-03-19 18:15:53 -0300

    Faster, better, and improved version

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
-/* global fixture test */
-const { Selector } = require('testcafe');
+const cheerio = require('cheerio');
+const axios = require('axios');
 
-fixture `Follow cards`
-  .page `https://www.deckshop.pro/card/by-arena`;
+const CARD_BY_ARENA_URL = 'https://www.deckshop.pro/card/by-arena';
+const CARD_DETAIL_URL = cardElementHref => `https://www.deckshop.pro${cardElementHref}`;
 
-test('card list', async t => {
-  const cards = Selector('.list-of-cards a');
-  const cardLinks = [];
+const scrap = url =>
+  new Promise((resolve, reject) => {
+    axios.get(url)
+      .then(result => {
+        console.log(`Accessed ${url}`);
+        resolve(cheerio.load(result.data));
+      })
+      .catch(error => {
+        reject({
+          message: `Error when fetching ${url}`,
+          error
+        });
+      });
+  });
 
-  for (let index = 0; index < await cards.count; index++) {
-    const cardLink = await cards.nth(index).getAttribute('href');
-    cardLinks.push(cardLink);
-  }
-
-  for (let index = 0; index < cardLinks.length; index++) {
-    const cardLink = await cardLinks[index];
-    await t.navigateTo(`https://www.deckshop.pro${cardLink}`);
-    const elixir = await Selector('h2 .elixir').with({ boundTestRun: t }).textContent;
-    console.log(`${cardLink}: ${elixir}`);
-  }
-
-  await t.expect(cardLinks.length).eql(82);
-});
+scrap(CARD_BY_ARENA_URL)
+  .then($ => Promise.all(
+    $('.list-of-cards a')
+      .map((i, el) => $(el).attr('href')).get()
+      .map(link => scrap(CARD_DETAIL_URL(link)))
+  ))
+  .then(cardPages =>
+    cardPages.map($ => ({
+      name: $('h2 > span').eq(0).text().trim(),
+      elixir: $('h2  > span').eq(1).text().trim(),
+      rarity: $('h2  > span').eq(2).text().trim(),
+      arena: $('h2  > span').eq(3).text().trim(),
+      attributes: $('table.table.table-inverse').eq(0).find('tbody tr').map((i, el) => ({
+        name: $(el).find('th').text().trim(),
+        value: $(el).find('td').text().trim()
+      })).get(),
+      flags: $('#properties + div a').map((i, el) => $(el).text().trim()).get(),
+      properties: $('#properties + div + div a').map((i, el) => $(el).text().trim()).get(),
+      counters: $('#counters + p + div a').map((i, el) => $(el).attr('href').trim()).get(),
+      countered: $('#countered + p + div a').map((i, el) => $(el).attr('href').trim()).get(),
+      synergies: $('#synergies + p + div a').map((i, el) => $(el).attr('href').trim()).get()
+    }))
+  )
+  .then(console.log)
+  .catch(console.error);
